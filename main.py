fc2633c3e50d7df2b33ef477237915ba65299ccc
Luciana <luciana.lucio@g.globo>
2022-06-08 21:53:27 -0300

    WIP feat: create users list and app.get method

--- main.py
+++ main.py
@@ -3,25 +3,62 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
-class Item(BaseModel):
+class User(BaseModel):
+    id: int
     name: str
-    price: float
-    is_offer: Union[bool, None] = None
+    password: str
+
+users = [
+    {
+        'id': 1,
+        'name': 'Luciana',
+        'password': '123456'
+    },
+    {
+        'id': 2,
+        'name': 'Maria',
+        'password': '234567'
+    },
+    {
+        'id': 3,
+        'name': 'Carlos',
+        'password': '654321'
+    },
+    {
+        'id': 4,
+        'name': 'Marcos',
+        'password': '894561'
+    }
+]
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+     return {"English Learning App"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/user/{user_id}")
+async def get_user(user_id: int, user: Union[str, None] = None):
+    search = list(filter(lambda x: x["id"] == user_id, users))
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+    if search == []:
+        return {'Error': 'Usuário não cadastrado'}
+
+    return {'Usuário': search[0]}
+
+@app.get("/user/{user_name}")
+async def get_user(user_name: str, user: Union[str, None] = None):
+    search = list(filter(lambda x: x["name"] == user_name, users))
+
+    if search == []:
+        return {'Error': 'Usuário não cadastrado'}
+
+    return {'Usuário': search[0]}
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_price": item.price, "item_id": item_id}
 
 # Criar 
+# @app.put
 # @app.post
 # @app.delete
 # @app.patch
